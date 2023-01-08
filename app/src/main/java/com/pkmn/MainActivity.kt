f8ffdcfa52968ace8201dc8e3113c9c8932cfb82
Ale <acasabona@sorint.com>
2023-01-09 00:43:13 +0100

    added 0,5x damage

--- app/src/main/java/com/pkmn/MainActivity.kt
+++ app/src/main/java/com/pkmn/MainActivity.kt
@@ -4,6 +4,7 @@ import android.R
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.pkmn.core.Type
@@ -27,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-/////////////////////// CREATION OF NAMES ARRAY ADAPTER FOR TYPE SELECTION //////////////////////
+
         //Create Array Adapter
         val adapter: ArrayAdapter<String> =
             ArrayAdapter<String>(this, R.layout.select_dialog_singlechoice, typeNamesArray)
@@ -40,18 +41,28 @@ class MainActivity : AppCompatActivity() {
         //Set the adapter
         acTextView1.setAdapter(adapter)
         acTextView2.setAdapter(adapter)
-/////////////////////////
+
 
         binding.CheckButton.setOnClickListener { onCheckClicked() }
 
+        binding.FirstTypeText.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) onTypeClicked() }
+        binding.SecondTypeText.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) onTypeClicked() }
+
+        var resultViewBase = TextView(this)
+        resultViewBase.setText("")
+
+        binding.BarLayout2.addView(resultViewBase)
+
     }
 
-///////////////// FUNCTION FOR WHEN CHECK BUTTON IS CLICKED ////////////////////////////////////
+
     private fun onCheckClicked() {
         var firstType = Type("", arrayOf());  var secondType = Type("", arrayOf())
         
         var firstTypeOk = false;  var secondTypeOk = false;  var allOK = false
-//// CONDITIONS //////
+
         if (binding.FirstTypeText.text.toString().equals("")) {
             firstType = noType
             firstTypeOk = true }
@@ -68,7 +79,7 @@ class MainActivity : AppCompatActivity() {
                 (binding.SecondTypeText.text.toString())) {
             errorDialogSameType()
             return }
-//////////////
+
         for (i in 0..17) {
             if (!firstTypeOk && binding.FirstTypeText.text.toString().
                 equals(typeArray[i].name)) {
@@ -79,28 +90,43 @@ class MainActivity : AppCompatActivity() {
                 equals(typeArray[i].name) ) {
                 secondType = typeArray[i]
                 secondTypeOk = true }
-//// CONDITION FOR BREAKING LOOP ////
+
             if (firstTypeOk && secondTypeOk) {
                 allOK = true
                 break }
-///////////////
+
         }
 
-        if (allOK) {  ////// CALL FUNCTION TO CALCULATE DAMAGE TAKEN ////////
-            binding.Result.visibility = View.VISIBLE
-            binding.ResultText.setText(calculateDamageTaken(firstType, secondType)) }
+        if (allOK) {
+
+            val theArrays = calculateDamageTaken(firstType, secondType)
+
+            val x4 = theArrays[0]; val x2 = theArrays[1]; val x0 = theArrays[2]
+
+            binding.BarLayout2.visibility = View.VISIBLE
+
+            if (x4.isNotEmpty() && x2.isNotEmpty() && x0.isNotEmpty()){
+
+
+            }
+
+        }
     }
-////////////////
 
-    /*private fun onFirstTypeClicked(){  /// SET RESULT BACK TO INVISIBLE ON CLICK ///
-        binding.Result.visibility = View.INVISIBLE
-        binding.ResultText.setText("")
-    }*/
 
-    /*private fun onSecondTypeClicked(){  /// SET RESULT BACK TO INVISIBLE ON CLICK ///
-        binding.Result.visibility = View.INVISIBLE
-        binding.ResultText.setText("")
-    }*/
+    private fun onTypeClicked(){
+        binding.BarLayout2.visibility = View.INVISIBLE
+    }
+
+    fun Type4xViewManagement(array:ArrayList<String>){
+        for (i in 0 until array.size){
+            if (i == 0){
+                binding.Type4xLabel1.text = array[0]
+            }
+
+        }
+
+    }
 
 //////// ERRORS //////////
     private fun errorDialogNoType() {
--- app/src/main/java/com/pkmn/core/Type.kt
+++ app/src/main/java/com/pkmn/core/Type.kt
@@ -46,49 +46,36 @@ val typeArray = arrayOf (Bug, Dark, Dragon, Electric, Fairy, Fighting, Fire, Fly
                          Ghost, Grass, Ground, Ice, Normal, Poison, Psychic, Rock, Steel, Water)
 
 /////// DAMAGE CALCULATION /////////
-fun calculateDamageTaken(type1: Type, type2: Type): String {
+fun calculateDamageTaken(type1: Type, type2: Type): ArrayList<ArrayList<String>> {
     var combination = Type("Combination", Array(18) { DamageTaken("", 1F) })
     val currentDamageTaken = DamageTaken("", 1F)
     for (i in 0..17) {
-        if (type1 == noType) {combination = type2; break}
-        if (type2 == noType) {combination = type1; break}
+        if (type1 == noType) {
+            combination = type2; break
+        }
+        if (type2 == noType) {
+            combination = type1; break
+        }
 
         currentDamageTaken.type = type1.damageTaken[i].type
-        currentDamageTaken.multiplier = type1.damageTaken[i].multiplier * type2.damageTaken[i].multiplier
+        currentDamageTaken.multiplier =
+            type1.damageTaken[i].multiplier * type2.damageTaken[i].multiplier
         combination.damageTaken[i].type = currentDamageTaken.type
         combination.damageTaken[i].multiplier = currentDamageTaken.multiplier
     }
 
     val x4 = arrayListOf<String>()
     val x2 = arrayListOf<String>()
+    val x05 = arrayListOf<String>()
     val x0 = arrayListOf<String>()
 
     for (i in 0..17) {
         if (combination.damageTaken[i].multiplier == 4F) x4.add(combination.damageTaken[i].type)
         if (combination.damageTaken[i].multiplier == 2F) x2.add(combination.damageTaken[i].type)
+        if (combination.damageTaken[i].multiplier == 0.5F) x05.add(combination.damageTaken[i].type)
         if (combination.damageTaken[i].multiplier == 0F) x0.add(combination.damageTaken[i].type)
     }
 
-    val x4Damage = "This Pokémon takes x4 damage by:\n$x4"
-    val x2Damage = "This Pokémon takes x2 damage by:\n$x2"
-    val x0Damage = "This Pokémon takes 0 damage by:\n$x0"
-    val x1Damage = "This Pokémon takes normal damage by everything else"
-
-////// RETURN CONDITIONS ////////
-    if (x4.isNotEmpty() && x2.isNotEmpty() && x0.isNotEmpty()) return "$x4Damage\n$x2Damage\n$x0Damage\n$x1Damage"
-
-    if (x2.isNotEmpty() && x0.isNotEmpty()) return "$x2Damage\n$x0Damage\n$x1Damage"
-
-    if (x4.isNotEmpty() && x2.isNotEmpty()) return "$x4Damage\n$x2Damage\n$x1Damage"
-
-    if (x4.isNotEmpty() && x0.isNotEmpty()) return "$x4Damage\n$x0Damage\n$x1Damage"
-
-    if (x0.isNotEmpty()) return "$x0Damage\n$x1Damage"
-
-    if (x2.isNotEmpty()) return "$x2Damage\n$x1Damage"
-
-    //else
-    return "$x4Damage\n$x1Damage"
-///////////
+    return arrayListOf(x4, x2, x05, x0)
 }
 ////////////
